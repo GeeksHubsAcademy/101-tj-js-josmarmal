5f14df55e87c62630f56b54db47c1fdb3a593e5e
JoseMarin <jmarinmalagon@gamil.com>
2021-04-30 14:55:46 +0200

    Update test01.js

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,26 @@
 function clasificaElementos(index) {
-    return [0,0,0];
+
+       // Arrays de resultados
+       var Npositivos = [];
+       var Nnegativos = [];
+       var Nzero = [];
+   
+       for(let i of index){
+           if(i > 0){ 
+               Npositivos.push(i);
+           } else if (i == 0) {
+               Nzero.push(i);
+           } else if (i < 0) {
+               Nnegativos.push(i);
+           };
+       };
+   
+       // Formateo de datos a retornar
+       let Npos = (Npositivos.length/index.length).toFixed(4);
+       let Nneg = (Nnegativos.length/index.length).toFixed(4);
+       let Nzer = (Nzero.length/index.length).toFixed(4);
+   
+       // return de datos formato predefinido
+       return [Npos,Nneg,Nzer];
 }
 module.exports = clasificaElementos;
